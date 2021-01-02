a374a6d7700872221db546c4784ed4f4ba033899
zachur <punsalang.zachary@gmail.com>
2021-01-01 17:37:23 -0800

    change state to next player 'X' and 'O'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+  /*
+  class Square extends React.Component {
     render() {
       return (
         <button
@@ -14,19 +15,33 @@ class Square extends React.Component {
       );
     }
   }
+  */
+
+  //Function Components
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
   
   class Board extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
   
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';  
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   
     renderSquare(i) {
@@ -39,8 +54,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       return (
         <div>
           <div className="status">{status}</div>
